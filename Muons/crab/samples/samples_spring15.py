fbb7087ced5fd43717918d5f1dbf0bc92e3adf32
Anna <kropiv@cern.ch>
2015-07-09 12:42:20 +0200

    working version to run

---
+++ Muons/crab/samples/samples_spring15.py
@@ -0,0 +1,22 @@
+########
+# 25 ns
+########
+#        CRAB task name       DAS name                                                                                    Latino name
+samples['25ns_DY_Spring15']        = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/AODSIM',  ['globalTag=MCRUN2_74_V9']]
+#samples['25ns_DY']        = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/AODSIM',  ['globalTag=MCRUN2_74_V9','outputFile=tnpZ_MC.root']]
+#samples['25ns_DY']        = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/AODSIM',  ['label=DY',        'id=0001', 'globalTag=MCRUN2_74_V9']]
+#samples['25ns_ggHToWWTo2L2Nu'] = ['/GluGluHToWWTo2L2Nu_M120_13TeV_powheg_JHUgen_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/AODSIM',         ['label=ggHToWWTo2L2Nu', 'id=0002', 'globalTag=MCRUN2_74_V9']]
+
+
+########
+# 50 ns
+########
+#        CRAB task name       DAS name                                                                                    Latino name
+samples['50ns_DY_Spring15']        = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/AODSIM',  ['globalTag=MCRUN2_74_V9A']]
+
+
+########
+# Alternative global configuration
+########
+# config.Data.outLFNDirBase = '/store/user/piedra/test'
+# config.Site.storageSite   = 'T2_ES_IFCA'
--- Muons/eff_tnp/fitMuonID_Simple.py
+++ Muons/eff_tnp/fitMuonID_Simple.py
@@ -1,16 +1,44 @@
 import FWCore.ParameterSet.Config as cms
 
+
+FitFunction = "vpvPlusExpo"
+
+
 process = cms.Process("TagProbe")
 
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.source = cms.Source("EmptySource")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
 process.TnP_Muon_ID = cms.EDAnalyzer("TagProbeFitTreeAnalyzer",
     ## Input, output 
     InputFileNames = cms.vstring(
-                                 #"root://eoscms//eos/cms/store/cmst3/user/botta/TnPtrees/tnpZ_Data.190456-193557.root",
-                                 "file:/afs/cern.ch/work/k/kropiv/MuonPOG/Samples/tnpZ_MC.root"
+                                 #"root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_50ns_DY_Spring15/50ns_DY_Spring15.root"
+                                 #"root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_50ns_DY_Spring15/150707_143432/0000/tnp_MC_99.root",
+                                 #"file:../crab/crab_projects_tnp/crab_50ns_DY_Spring15/results/tnp_MC.root",  
+                                 #DY 25 ns
+                                 #"file:../crab/crab_projects_tnp/crab_25ns_DY_Spring15/results/tnp_MC.root",  
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_100.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_101.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_102.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_103.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_104.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_105.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_106.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_107.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_108.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_109.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_110.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_111.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_112.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_113.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_114.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_115.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_116.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_117.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_118.root",
+                                 "root://eoscms//eos/cms/store/group/phys_higgs/cmshww/kropiv/TnP_Muons/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_DY_Spring15/150707_143420/0000/tnp_MC_119.root",
                                  ), ## can put more than one
     ## copy locally to be faster: xrdcp root://eoscms//eos/cms/store/cmst3/user/botta/TnPtrees/tnpZ_Data.190456-193557.root $PWD/tnpZ_Data.190456-193557.root
     ## and then set InputFileNames = cms.vstring("tnpZ_Data.190456-193557.root"), 
@@ -22,29 +50,75 @@ process.TnP_Muon_ID = cms.EDAnalyzer("TagProbeFitTreeAnalyzer",
         mass   = cms.vstring("Tag-muon Mass", "76", "125", "GeV/c^{2}"),
         pt     = cms.vstring("muon p_{T}", "0", "1000", "GeV/c"),
         abseta = cms.vstring("muon |#eta|", "0", "2.5", ""),
+        phi = cms.vstring("muon |#phi|", "-3.142", "3.142", ""),
         pair_dz = cms.vstring("#Deltaz between two muons", "-100", "100", "cm"),
 
     ),
     ## Flags you want to use to define numerator and possibly denominator
     Categories = cms.PSet(
         PF = cms.vstring("PF Muon", "dummy[pass=1,fail=0]"),
-        tag_IsoMu24_eta2p1 = cms.vstring("PF Muon", "dummy[pass=1,fail=0]"),
+        tag_IsoMu24_eta2p1 = cms.vstring("IsoMu24_eta2p1 tag Muon", "dummy[pass=1,fail=0]"),
+        Tight2012 = cms.vstring("Tight2012 Muon", "dummy[pass=1,fail=0]"),
+        tag_Tight2012 = cms.vstring("Tight 2012 tag Muon", "dummy[pass=1,fail=0]"),
+        Medium = cms.vstring("Medium Muon", "dummy[pass=1,fail=0]"),
     ),
     ## What to fit
     Efficiencies = cms.PSet(
-        PF_pt_eta = cms.PSet(
+    #    PF_pt_eta = cms.PSet(
+    #        UnbinnedVariables = cms.vstring("mass"),
+    #        EfficiencyCategoryAndState = cms.vstring("PF", "pass"), ## Numerator definition
+    #        BinnedVariables = cms.PSet(
+    #            ## Binning in continuous variables
+    #            pt     = cms.vdouble( 10, 20, 30, 40, 60, 100 ),
+    #            abseta = cms.vdouble( 0.0, 1.2, 2.4),
+    #            ## flags and conditions required at the denominator, 
+    #            tag_IsoMu24_eta2p1 = cms.vstring("pass"), ## i.e. use only events for which this flag is true
+    #            pair_dz = cms.vdouble(-1.,1.)             ## and for which -1.0 < dz < 1.0
+    #        ),
+    #        BinToPDFmap = cms.vstring("vpvPlusExpo"), ## PDF to use, as defined below
+    #    ),
+########
+        pt = cms.PSet(
             UnbinnedVariables = cms.vstring("mass"),
-            EfficiencyCategoryAndState = cms.vstring("PF", "pass"), ## Numerator definition
+            EfficiencyCategoryAndState = cms.vstring("Tight2012", "pass"), ## Numerator definition
+            #EfficiencyCategoryAndState = cms.vstring("PF", "pass"), ## Numerator definition
             BinnedVariables = cms.PSet(
-                ## Binning in continuous variables
-                pt     = cms.vdouble( 10, 20, 30, 40, 60, 100 ),
-                abseta = cms.vdouble( 0.0, 1.2, 2.4),
-                ## flags and conditions required at the denominator, 
                 tag_IsoMu24_eta2p1 = cms.vstring("pass"), ## i.e. use only events for which this flag is true
-                pair_dz = cms.vdouble(-1.,1.)             ## and for which -1.0 < dz < 1.0
+                tag_Tight2012 = cms.vstring("pass"),
+                abseta = cms.vdouble(0, 2.4),
+                pt = cms.vdouble(10, 20, 30, 40, 60, 100),
             ),
-            BinToPDFmap = cms.vstring("vpvPlusExpo"), ## PDF to use, as defined below
-        )
+            BinToPDFmap = cms.vstring(FitFunction)
+        ),
+########
+
+        abseta = cms.PSet(
+            UnbinnedVariables = cms.vstring("mass"),
+            EfficiencyCategoryAndState = cms.vstring("Tight2012", "pass"), ## Numerator definition
+            BinnedVariables = cms.PSet(
+                tag_IsoMu24_eta2p1 = cms.vstring("pass"), ## i.e. use only events for which this flag is true
+                tag_Tight2012 = cms.vstring("pass"),
+                abseta = cms.vdouble(0., 0.8, 1.2, 2.1, 2.4),
+                pt = cms.vdouble(10, 1000),
+            ),
+            BinToPDFmap = cms.vstring(FitFunction)
+        ),
+########
+        phi = cms.PSet(
+           EfficiencyCategoryAndState = cms.vstring("Tight2012", "pass"),
+            UnbinnedVariables = cms.vstring("mass"),
+            BinnedVariables = cms.PSet(
+                tag_IsoMu24_eta2p1 = cms.vstring("pass"), ## i.e. use only events for which this flag is true
+                tag_Tight2012 = cms.vstring("pass"),
+                abseta = cms.vdouble(0., 2.4),
+                pt = cms.vdouble(10, 1000),
+                phi = cms.vdouble(-3.142, -1.5, 0., 1.5, 3.142), # for GI or CI MC only
+            ),
+            BinToPDFmap = cms.vstring(FitFunction)
+        ),
+
+########
+
     ),
     ## PDF for signal and background (double voigtian + exponential background)
     PDFs = cms.PSet(
@@ -81,32 +155,95 @@ process.TnP_Muon_Iso = process.TnP_Muon_ID.clone(
     ),
     ## What to fit
     Efficiencies = cms.PSet(
-        Iso_vtx_tight = cms.PSet(
+       # Iso_vtx_tight = cms.PSet(
+       #     UnbinnedVariables = cms.vstring("mass"),
+       #     EfficiencyCategoryAndState = cms.vstring("PFIsoTight", "below"), ## variable is below cut value 
+       #     BinnedVariables = cms.PSet(
+       #         tag_nVertices = cms.vdouble(0.5,4.5,8.5,12.5,16.5,20.5,24.5,30.5), 
+       #         #PF = cms.vstring("pass"),                 ## 
+       #         Tight2012 = cms.vstring("pass"),
+       #         tag_IsoMu24_eta2p1 = cms.vstring("pass"), ## tag trigger matched
+       #         #pair_dz = cms.vdouble( -1.,1. ),          ## and for which -1.0 < dz < 1.0
+       #         pt     = cms.vdouble( 25,  100 ),
+       #         abseta = cms.vdouble( 0.0, 2.4 ),
+       #     ),
+       #     BinToPDFmap = cms.vstring("vpvPlusExpo"), ## PDF to use, as defined below
+       # ),
+       # Iso_vtx_loose = cms.PSet(
+       #     UnbinnedVariables = cms.vstring("mass"),
+       #     EfficiencyCategoryAndState = cms.vstring("PFIsoLoose", "below"), ## variable is below cut value 
+       #     BinnedVariables = cms.PSet(
+       #         tag_nVertices = cms.vdouble(0.5,4.5,8.5,12.5,16.5,20.5,24.5,30.5), 
+       #         #PF = cms.vstring("pass"),                 ## 
+       #         Tight2012 = cms.vstring("pass"),
+       #         tag_IsoMu24_eta2p1 = cms.vstring("pass"), ## tag trigger matched
+       #         #pair_dz = cms.vdouble( -1.,1. ),          ## and for which -1.0 < dz < 1.0
+       #         pt     = cms.vdouble( 25,  100 ),
+       #         abseta = cms.vdouble( 0.0, 2.4 ),
+       #     ),
+       #     BinToPDFmap = cms.vstring("vpvPlusExpo"), ## PDF to use, as defined below
+       # ),
+        ##############
+        Iso_pt_tight = cms.PSet(
             UnbinnedVariables = cms.vstring("mass"),
             EfficiencyCategoryAndState = cms.vstring("PFIsoTight", "below"), ## variable is below cut value 
             BinnedVariables = cms.PSet(
-                tag_nVertices = cms.vdouble(0.5,4.5,8.5,12.5,16.5,20.5,24.5,30.5), 
-                PF = cms.vstring("pass"),                 ## 
+                pt = cms.vdouble(10, 20, 30, 40, 60, 100),
+                #PF = cms.vstring("pass"),                 ## 
+                Tight2012 = cms.vstring("pass"),
                 tag_IsoMu24_eta2p1 = cms.vstring("pass"), ## tag trigger matched
-                pair_dz = cms.vdouble( -1.,1. ),          ## and for which -1.0 < dz < 1.0
-                pt     = cms.vdouble( 25,  100 ),
+                #pair_dz = cms.vdouble( -1.,1. ),          ## and for which -1.0 < dz < 1.0
                 abseta = cms.vdouble( 0.0, 2.4 ),
             ),
             BinToPDFmap = cms.vstring("vpvPlusExpo"), ## PDF to use, as defined below
         ),
-        Iso_vtx_loose = cms.PSet(
+        ##############
+        Iso_abseta_tight = cms.PSet(
+            UnbinnedVariables = cms.vstring("mass"),
+            EfficiencyCategoryAndState = cms.vstring("PFIsoTight", "below"), ## variable is below cut value 
+            BinnedVariables = cms.PSet(
+                abseta = cms.vdouble( 0.0, 0.8, 1.2, 2.1, 2.4 ),
+                #PF = cms.vstring("pass"),                 ## 
+                Tight2012 = cms.vstring("pass"),
+                tag_IsoMu24_eta2p1 = cms.vstring("pass"), ## tag trigger matched
+                #pair_dz = cms.vdouble( -1.,1. ),          ## and for which -1.0 < dz < 1.0
+                pt = cms.vdouble(10,1000),
+            ),
+            BinToPDFmap = cms.vstring("vpvPlusExpo"), ## PDF to use, as defined below
+        ),
+
+
+        ##############
+        Iso_pt_loose = cms.PSet(
             UnbinnedVariables = cms.vstring("mass"),
             EfficiencyCategoryAndState = cms.vstring("PFIsoLoose", "below"), ## variable is below cut value 
             BinnedVariables = cms.PSet(
-                tag_nVertices = cms.vdouble(0.5,4.5,8.5,12.5,16.5,20.5,24.5,30.5), 
-                PF = cms.vstring("pass"),                 ## 
+                pt = cms.vdouble(10, 20, 30, 40, 60, 100),
+                #PF = cms.vstring("pass"),                 ## 
+                Tight2012 = cms.vstring("pass"),
                 tag_IsoMu24_eta2p1 = cms.vstring("pass"), ## tag trigger matched
-                pair_dz = cms.vdouble( -1.,1. ),          ## and for which -1.0 < dz < 1.0
-                pt     = cms.vdouble( 25,  100 ),
+                #pair_dz = cms.vdouble( -1.,1. ),          ## and for which -1.0 < dz < 1.0
                 abseta = cms.vdouble( 0.0, 2.4 ),
             ),
             BinToPDFmap = cms.vstring("vpvPlusExpo"), ## PDF to use, as defined below
         ),
+        ##############
+        Iso_abseta_loose = cms.PSet(
+            UnbinnedVariables = cms.vstring("mass"),
+            EfficiencyCategoryAndState = cms.vstring("PFIsoLoose", "below"), ## variable is below cut value 
+            BinnedVariables = cms.PSet(
+                abseta = cms.vdouble( 0.0, 0.8, 1.2, 2.1, 2.4 ),
+                #PF = cms.vstring("pass"),                 ## 
+                Tight2012 = cms.vstring("pass"),
+                tag_IsoMu24_eta2p1 = cms.vstring("pass"), ## tag trigger matched
+                #pair_dz = cms.vdouble( -1.,1. ),          ## and for which -1.0 < dz < 1.0
+                pt = cms.vdouble(10,1000),
+            ),
+            BinToPDFmap = cms.vstring("vpvPlusExpo"), ## PDF to use, as defined below
+        ),
+
+
+        ##############
     ),
 )
 
