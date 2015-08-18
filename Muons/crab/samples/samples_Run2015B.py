2904e801cde89b0db7460c18d8719bdb5cc2d63f
Anna <kropiv@cern.ch>
2015-08-18 13:13:18 +0200

    add data Run2015B sample

---
+++ Muons/crab/samples/samples_Run2015B.py
@@ -0,0 +1,37 @@
+########
+# 25 ns
+########
+#        CRAB task name       DAS name                                                                                    Latino name
+#samples['25ns_DY_Spring15']        = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/AODSIM',  ['globalTag=MCRUN2_74_V9']]
+#samples['25ns_DY']        = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/AODSIM',  ['globalTag=MCRUN2_74_V9','outputFile=tnpZ_MC.root']]
+#samples['25ns_DY']        = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/AODSIM',  ['label=DY',        'id=0001', 'globalTag=MCRUN2_74_V9']]
+#samples['25ns_ggHToWWTo2L2Nu'] = ['/GluGluHToWWTo2L2Nu_M120_13TeV_powheg_JHUgen_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/AODSIM',         ['label=ggHToWWTo2L2Nu', 'id=0002', 'globalTag=MCRUN2_74_V9']]
+
+
+########
+# 50 ns
+########
+#        CRAB task name       DAS name                                                                                    Latino name
+#samples['50ns_DY_Spring15']        = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/AODSIM',  ['globalTag=MCRUN2_74_V9A']]
+
+
+########
+# 50 ns DATA:
+########
+ 
+samples['ZeroBias_Run2015B']         = ['/ZeroBias/Run2015B-PromptReco-v1/RECO',  ['globalTag=74X_dataRun2_Prompt_v1']]
+#samples['ZeroBias1_Run2015B']        = ['/ZeroBias1/Run2015B-PromptReco-v1/RECO',  ['globalTag=74X_dataRun2_Prompt_v1']]
+#samples['ZeroBias2_Run2015B']        = ['/ZeroBias2/Run2015B-PromptReco-v1/RECO',  ['globalTag=74X_dataRun2_Prompt_v1']]
+#samples['ZeroBias3_Run2015B']        = ['/ZeroBias3/Run2015B-PromptReco-v1/RECO',  ['globalTag=74X_dataRun2_Prompt_v1']]
+#samples['ZeroBias4_Run2015B']        = ['/ZeroBias4/Run2015B-PromptReco-v1/RECO',  ['globalTag=74X_dataRun2_Prompt_v1']]
+#samples['ZeroBias5_Run2015B']        = ['/ZeroBias5/Run2015B-PromptReco-v1/RECO',  ['globalTag=74X_dataRun2_Prompt_v1']]
+#samples['ZeroBias6_Run2015B']        = ['/ZeroBias6/Run2015B-PromptReco-v1/RECO',  ['globalTag=74X_dataRun2_Prompt_v1']]
+#samples['ZeroBias7_Run2015B']        = ['/ZeroBias7/Run2015B-PromptReco-v1/RECO',  ['globalTag=74X_dataRun2_Prompt_v1']]
+#samples['ZeroBias8_Run2015B']        = ['/ZeroBias8/Run2015B-PromptReco-v1/RECO',  ['globalTag=74X_dataRun2_Prompt_v1']]
+
+
+########
+# Alternative global configuration
+########
+# config.Data.outLFNDirBase = '/store/user/piedra/test'
+# config.Site.storageSite   = 'T2_ES_IFCA'
